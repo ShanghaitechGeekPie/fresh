2c0ec673275136f70619e16171db87921c909d61
Masquentin <linhy@shanghaitech.edu.cn>
2022-07-15 01:16:12 +0800

    switch to 2022

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import MainLayout from '../layouts/MainLayout/MainLayout';
 import parseMd from '../parseMd/parseMd';
 import NotFound from './NotFound';
 
-const markdown = require('../markdown/上海科技大学2021新生手册_jsdeliver.md')
+const markdown = require('../markdown/上海科技大学2022新生手册.md')
 
 var parseMdt = new parseMd(markdown);
 var data = parseMdt.render();
