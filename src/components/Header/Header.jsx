6f90a087757574c81c7d013eec38186bb497ecb8
MaoTechh <MaoTechMail@gmail.com>
2025-06-04 17:29:10 +0800

    Fix: Change to the correct welcome page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -21,7 +21,7 @@ class Header extends React.Component {
       )
 
     menu.push(
-      <a href="http://res.shanghaitech.edu.cn/yingxin/yingxin.html" target="_blank">
+      <a href="https://fresh.shanghaitech.edu.cn" target="_blank">
         <li>
           <small>返回 迎新网</small>
         </li>
